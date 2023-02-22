f09e5b31835cbf593b35085e49697a90393d435c
Anton1919 <bf3kenzo@mail.ru>
2023-02-22 11:06:19 +0300

    ref

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -16,7 +16,6 @@ function HW11() {
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
     const change = (event: Event, value: number | number[]) => {
-
         if(Array.isArray(value)) {
             setValue1(value[0])
             setValue2(value[1])
@@ -25,6 +24,7 @@ function HW11() {
         }
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
     }
+
 
     return (
         <div id={'hw11'}>
